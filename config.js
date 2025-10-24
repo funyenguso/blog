750176377e0be24edc15a1aac10c4ab971b5b78b
funyenguso <funyenguso@gmail.com>
2025-10-24 15:07:41 +0900

    사용자이름 변경

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
+    username: "펀연구소",
     company: "weniv",
     position: "CEO",
     img: "img/user/profile-licat.png",
